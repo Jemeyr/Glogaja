bbd82c317fb356060956c1f176c74d3c329e86b3
Jemeyr <jmohr@uvic.ca>
2014-01-25 18:09:21 -0800

    added a candy and some code so that you can stick to candies

---
+++ Glogaja/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = target.position;
+		transform.position += Vector3.up * 10.0f;
+
+
+
+	}
+}
--- Glogaja/Assets/Scripts/PlayerMove.cs
+++ Glogaja/Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMove : MonoBehaviour {
 
+
 	void Start () {
 	
 	}
@@ -28,8 +29,19 @@ public class PlayerMove : MonoBehaviour {
 		//slow down eventually
 		rigidbody.AddForce(-0.1f * rigidbody.velocity);
 		rigidbody.AddTorque(-0.1f * rigidbody.angularVelocity);
-		
-
 
 	}
+
+	void OnTriggerEnter(Collider other){
+		GameObject part = other.gameObject;
+
+		part.transform.parent = transform;
+
+		FixedJoint joint = transform.gameObject.AddComponent<FixedJoint>();
+		joint.connectedBody = part.rigidbody;
+
+		//add this to a list or map or something so it updates shooters?
+
+	}
+
 }
