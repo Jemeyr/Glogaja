6a409d99d761a4143e60d738e341043299b7bf40
Jemeyr <jmohr@uvic.ca>
2014-01-26 00:17:19 -0800

    added particles for engines

--- Glogaja/Assets/Scripts/Boost.cs
+++ Glogaja/Assets/Scripts/Boost.cs
@@ -5,12 +5,30 @@ public class Boost : Effect {
 
 	public float power = 1.0f;
 
+	private ParticleSystem emitter;
+
+	
+	public void Awake(){
+		emitter = GetComponentInChildren<ParticleSystem>();	
+		emitter.enableEmission = false;
+	}
+
+
 	public override void Run () {
 		if (Input.GetKey(KeyCode.W)){
+			emitter.startSpeed = 5;
+			emitter.enableEmission = true;
+
 			rigidbody.AddForce(power * transform.forward);
 		}
 		else if (Input.GetKey(KeyCode.S)){
+			emitter.startSpeed = -5;
+			emitter.enableEmission = true;
+
 			rigidbody.AddForce(power * -transform.forward);
+		}
+		else{
+			emitter.enableEmission = false;
 		}
 	}
 
--- Glogaja/Assets/Scripts/PlayerMove.cs
+++ Glogaja/Assets/Scripts/PlayerMove.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [AddComponentMenu("PlayerStuff/PlayerMove")]
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Sticky))]
 public class PlayerMove : MonoBehaviour {
 	private Sticky sticky;
+	private List<ParticleSystem> particleSystems;
+
 
 	public float positionSlowdown = 0.9f;
 	public float rotationSlowdown = 0.9f;
@@ -16,6 +19,13 @@ public class PlayerMove : MonoBehaviour {
 
 	void Start () {
 		sticky = GetComponent<Sticky>();
+
+		particleSystems = new List<ParticleSystem>();
+
+		ParticleSystem[] systems = GetComponentsInChildren<ParticleSystem>();
+		foreach(ParticleSystem system in systems){
+			particleSystems.Add (system);
+		}
 	}
 
 
@@ -25,12 +35,27 @@ public class PlayerMove : MonoBehaviour {
 		//move
 		if (Input.GetKey(KeyCode.W)){
 			rigidbody.AddForce(power * transform.forward);
+
+			foreach(ParticleSystem s in particleSystems){
+				s.startSpeed = 5;
+				s.enableEmission = true;
+			}
 		}
 		else if (Input.GetKey(KeyCode.S)){
 			rigidbody.AddForce(power * -transform.forward);
+
+			foreach(ParticleSystem s in particleSystems){
+				s.startSpeed = -5;
+				s.enableEmission = true;
+			}
 		}
 		else{
 			rigidbody.AddForce(-positionSlowdown * rigidbody.velocity);
+
+			
+			foreach(ParticleSystem s in particleSystems){
+				s.enableEmission = false;
+			}
 		}
 
 		//turn
@@ -49,8 +74,17 @@ public class PlayerMove : MonoBehaviour {
 			Debug.Log(sticky.GetPower());
 		}
 
-
 		sticky.Run();
 	}
+
+
+	public void AddEngine(ParticleSystem s){
+		particleSystems.Add(s);
+	}
+
+	public void RemoveEngine(ParticleSystem s){
+		particleSystems.Remove(s);
+	}
+
 
 }
