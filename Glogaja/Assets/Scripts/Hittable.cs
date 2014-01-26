8349d901d74bde903ad3644d7aaad126889498c6
beyamor <beyamor@gmail.com>
2014-01-26 14:22:40 -0800

    Hitting an enemy

---
+++ Glogaja/Assets/Scripts/Hittable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hittable : MonoBehaviour {
+	
+	public bool destroyOnHit = true;
+	
+	public void hit() {
+
+		Debug.Log ("Hit!");
+	}
+}
---
+++ Glogaja/Assets/Scripts/Hitter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hitter : MonoBehaviour
+{
+	public bool destroyOnHit = true;
+	
+	void OnCollisionEnter(Collision collision){
+
+		var other = collision.gameObject;
+		var hittable = other.GetComponent<Hittable> ();
+		
+		if (hittable == null)
+			return;
+		
+		hittable.hit();
+		
+		if (hittable.destroyOnHit)
+			Destroy (hittable.gameObject);
+		
+		if (destroyOnHit)
+			Destroy (gameObject);
+	}
+}
