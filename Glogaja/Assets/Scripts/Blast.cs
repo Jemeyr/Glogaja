763e8e7c4f8f70c5c88d4de66366cefb6fdd7c58
beyamor <beyamor@gmail.com>
2014-01-26 11:49:13 -0800

    Started writing a blaster script

---
+++ Glogaja/Assets/Scripts/Blast.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blast : Effect {
+
+	public override void Run ()
+	{
+		if (Input.GetKey (KeyCode.Space)) {
+
+			Debug.Log("Blasting!");
+		}
+	}
+}
