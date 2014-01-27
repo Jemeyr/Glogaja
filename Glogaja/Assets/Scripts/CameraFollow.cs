f69c9b3b37ab8d63ccf51690ee4c7f1389551250
Jemeyr <jmohr@uvic.ca>
2014-01-26 17:39:17 -0800

    added zoom in/out on keyboard buttons, should probably use velocity or size though

--- Glogaja/Assets/Scripts/CameraFollow.cs
+++ Glogaja/Assets/Scripts/CameraFollow.cs
@@ -9,6 +9,13 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		if(Input.GetKey(KeyCode.UpArrow)){
+			distance *= 0.99f;
+		} else if(Input.GetKey(KeyCode.DownArrow)){
+			distance *= 1.01f;
+		}
+
+
 		transform.position = target.position + Vector3.up * distance;
 		transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
 
