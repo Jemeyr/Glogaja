3a34d33bb686015b85a2692d453363400cf5c666
beyamor <beyamor@gmail.com>
2014-01-26 17:31:52 -0800

    A HittableSticky component

---
+++ Glogaja/Assets/Scripts/hitting/HittableSticky.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HittableSticky : Hittable {
+
+	public override void hit (GameObject hitter)
+	{
+		var sticky = GetComponent<Sticky> ();
+
+		if (sticky == null)
+						return;
+
+		sticky.Unstick();
+	}
+}
