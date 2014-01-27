0019822e7c5979d43e0c28668543c8821a9e75a9
Jemeyr <jmohr@uvic.ca>
2014-01-26 17:02:02 -0800

    small fix to enemy ship and now things drift around

---
+++ Glogaja/Assets/Scripts/FlyAway.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyAway : MonoBehaviour {
+
+	public float speed = 0.05f;
+
+	private Vector3 direction = Vector3.zero;
+
+	void Start () {
+		float x = Random.Range(-1.0f, 1.0f);
+		float z = Random.Range(-1.0f, 1.0f);
+
+		direction = new Vector3(x,0.0f,z);
+		direction.Normalize();
+
+
+		if(rigidbody != null){
+			rigidbody.AddForce(direction * 100 * speed);
+		}
+
+	}
+
+}
