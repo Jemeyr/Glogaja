b58d3f07e4a0132c7cab81d7d4560b2972d0da67
Jemeyr <jmohr@uvic.ca>
2014-01-27 23:55:24 -0800

    cleaned up the horrible hierarchy and added more candy generation

--- Glogaja/Assets/Scripts/JellybeanGenerator.cs
+++ Glogaja/Assets/Scripts/JellybeanGenerator.cs
@@ -5,13 +5,14 @@ public class JellybeanGenerator : MonoBehaviour {
 
 	public Material[] colors;
 	public GameObject jellybean;
+	public Vector3 offset = Vector3.zero;
 
 	//TODO: make this not generate a line
 	void Start () {
 
 
 		for(int i = -50; i < 50; i++){
-			Vector3 place = new Vector3(i*3, 0, 20);
+			Vector3 place = new Vector3(i*3, 0, 0) + offset;
 			Quaternion rot = new Quaternion();
 
 			GameObject current = Instantiate(jellybean, place, rot) as GameObject;
